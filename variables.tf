f2448135814bcc0a0c57aa9626d57a0ff82a05c9
Shubham Kumar Sahu <shubhamsahu410@gmail.com>
2025-08-16 22:03:27 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "Primary AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "secondary_region" {
+  description = "Secondary AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Amazon Linux AMI ID for us-east-1"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
+}
